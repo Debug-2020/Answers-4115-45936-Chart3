e11b9031caa52d5bf64e83d7dd08e6df42c4a5c0
Debug-2020 <debug2020@protonmail.com>
2020-01-02 15:29:52 +0800

    4115-45936-Chart3-result1575169332916

--- src/org/jfree/data/time/TimeSeries.java
+++ src/org/jfree/data/time/TimeSeries.java
@@ -1055,8 +1055,6 @@ public class TimeSeries extends Series implements Cloneable, Serializable {
         }
         TimeSeries copy = (TimeSeries) super.clone();
         copy.data = new java.util.ArrayList();
-        copy.minY = Double.NaN;
-        copy.maxY = Double.NaN;
         if (this.data.size() > 0) {
             for (int index = start; index <= end; index++) {
                 TimeSeriesDataItem item
@@ -1070,6 +1068,7 @@ public class TimeSeries extends Series implements Cloneable, Serializable {
                 }
             }
         }
+        copy.findBoundsByIteration();
         return copy;
     }
 
